8472a8861c5069f9105b16b81ef56944f91ce2da
Tanishk <justinakun18@gmail.com>
2026-07-23 18:47:29 +0900

    Add model evaluation and visualization

--- Assignment-3.py
+++ Assignment-3.py
@@ -5,7 +5,13 @@
 
 # Import Required Libraries
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # ============================================================
 # Task 1: Data Understanding
